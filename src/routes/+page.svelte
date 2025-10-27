3ed0181ac50c01362878828724f4f17c71c117dc
Zag <za12ew44zz_999@hotmail.com>
2025-10-27 11:18:17 +0700

    add krathong

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,36 @@
 <script>
-
+  import krathongImg from '$lib/assets/krathong.png'
+  import { fly } from 'svelte/transition';
 </script>
-
+<div class="moon"></div>
+<div class="floor"></div>
+<img src={krathongImg} alt="krathong" class="krathong"  />
 <div class="water-container">
   <div class="wave3"></div>
 </div>
 
 <style>
+
+  .floor{
+    width: 100%;
+    height: 2rem;
+    position: absolute;
+    bottom: 14rem;
+    
+
+    
+    background-color: antiquewhite;
+  }
+
+  .krathong {
+  position: absolute; 
+  bottom: 12rem; 
+ 
+
+  width: 8rem; 
+  z-index: 9999; 
+  animation: bobing 10s linear infinite,floatLeftToRight 40s linear infinite;
+  }
 
 
   .water-container {
@@ -18,9 +42,28 @@
     left: 0;
     background-color: #004d80; 
     z-index: 999;
-   animation: bgColorChange 40s linear infinite alternate;
+    animation: bgColorChange 40s linear infinite alternate;
     
   }
+  
+  
+
+  .moon{
+    width: 2rem;
+    height: 2rem;
+    position: absolute;
+   width: 5vw; 
+    height: 5vw;
+    top: 3vh; 
+    right: 3vw; 
+    width: 60px;       
+    height: 60px;
+    background-color: white;
+    box-shadow: 0 0 15px 5px rgba(255, 255, 200, 0.7);
+    border-radius: 50%;
+  }
+
+
   .water-container::before{
     content: "";
     display:block;
@@ -73,5 +116,21 @@
     0% { background-color: #004d80; }
     50% { background-color: #0d6d9d; } 
     100% { background-color: #004d80; } 
+  }
+
+  @keyframes floatLeftToRight{
+    0% {
+    left: -10%;
+  }
+  100% {
+    left: 110%;
+  }
+  
+  }
+
+  @keyframes bobing{
+  0% { transform: translateY(0) rotate(-5deg); }
+  50% { transform: translateY(-0.5rem) rotate(5deg); } 
+  100% { transform: translateY(0) rotate(-5deg); }
   }
 </style>
